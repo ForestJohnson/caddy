90285947df1ce51dd9f10dc7c3449d4909c13470
forest <forest.n.johnson@gmail.com>
2021-10-24 14:41:41 -0500

    update publish script

--- picopublish.sh
+++ picopublish.sh
@@ -1,22 +1,23 @@
 #!/bin/bash -e
 
+tag="0.0.0"
+if git describe --tags --abbrev=0 > /dev/null 2>&1 ; then
+  tag="$(git describe --tags --abbrev=0)"
+fi
+version="$tag-$(git rev-parse --short HEAD)-$(hexdump -n 2 -ve '1/1 "%.2x"' /dev/urandom)"
+
 function build() {
   GOOS=$1
   GOARCH=$2
-  tag="0.0.0"
-  if git describe --tags --abbrev=0 > /dev/null 2>&1 ; then
-    tag="$(git describe --tags --abbrev=0)"
-  fi
-  version="$tag-$(git rev-parse --short HEAD)-$(hexdump -n 2 -ve '1/1 "%.2x"' /dev/urandom)"
 
   echo "building version: $version"
 
-	rm -rf build
-	mkdir build
+  rm -rf build
+  mkdir build
 
-	go build -v -tags 'osusergo netgo'  -ldflags='-extldflags=-static'  -o build/caddy ./cmd/caddy
+  GOOS=$GOOS GOARCH=$GOARCH go build -tags 'osusergo netgo'  -ldflags='-extldflags=-static'  -o build/caddy ./cmd/caddy
 
-	sha256sum build/caddy
+  sha256sum build/caddy
 
   gzip_file_name="caddy-$version-$GOOS-$GOARCH.gz"
 
@@ -29,6 +30,9 @@ function build() {
   echo "https://picopublish.sequentialread.com/files/$gzip_file_name"
 }
 
-#build arm
+build linux arm
+build linux arm64
 build linux amd64
+#build windows amd64
+#build darwin amd64
 #build arm64
